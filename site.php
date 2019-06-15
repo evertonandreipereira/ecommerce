b444d23f6c964c6cebe5b57683dd3bf391250c93
evertonandreipereira <evertonandreipereira@hotmail.com>
2019-06-15 15:59:13 -0300

    Carrinho Calculo de Frete

--- site.php
+++ site.php
@@ -65,7 +65,8 @@ $app->get("/cart", function(){
 	$page = new Page();
 	$page->setTpl("cart", [
 		'cart'=>$cart->getValues(),
-		'products'=>$cart->getProducts()
+		'products'=>$cart->getProducts(),
+		'error'=>Cart::getMsgError()
 	]);
 
 });
@@ -116,6 +117,14 @@ $app->get("/cart/:idproduct/remove", function($idproduct){
 
 	$cart->removeProduct($product, true);
 
+	header("Location: /cart");
+	exit;
+
+});
+
+$app->post("/cart/freight", function(){
+	$cart = Cart::getFromSession();
+	$cart->setFreight($_POST['zipcode']);
 	header("Location: /cart");
 	exit;
 
