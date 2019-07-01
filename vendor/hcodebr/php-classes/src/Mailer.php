44e75c678394b6cbb08d556989162e3d1f2a3eb7
evertonandreipereira <evertonandreipereira@hotmail.com>
2019-07-01 16:46:12 -0300

    Pedidos

--- vendor/hcodebr/php-classes/src/Mailer.php
+++ vendor/hcodebr/php-classes/src/Mailer.php
@@ -7,7 +7,7 @@ use Rain\Tpl;
 class Mailer {
 
 	const USERNAME = "evertonandreipereira@hotmail.com";
-	const PASSWORD = "*********";
+	const PASSWORD = "**********";
 	const NAME_FROM = "Hcode Store";
 
 	private $mail;
